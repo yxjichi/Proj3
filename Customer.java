4b1e480c514842c550efd924980def60e9eb1be5
r_byfield <65741150+yxjichi@users.noreply.github.com>
2025-04-08 16:42:14 -0500

    building out customer

--- Customer.java
+++ Customer.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class Customer implements Comparable //<Customer>
 {
     private String name;
     private int loyalty;
-    private Vehicle[] ownership; 
+    private ArrayList<Vehicle> ownership; 
+
+
+    public Customer(String name)
+    {
+        loyalty = 0;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public int CompareTo(Customer a){
+        return this.name.compareTo(a.getName());
+    }
 }
