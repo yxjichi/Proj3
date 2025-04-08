4c6ae1210873c1d1bd86dd465c2ac80fadc7e755
r_byfield <65741150+yxjichi@users.noreply.github.com>
2025-04-08 16:21:02 -0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,6 @@
+public class Customer implements Comparable //<Customer>
+{
+    private String name;
+    private int loyalty;
+    private Vehicle[] ownership; 
+}
---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Vehicle extends Comparable//<Vehicle>
+{
+    private String model;
+    private String plate;
+    private int mileage;
+    private ArrayList<Service> serviceHistory;
+
+}
